fccb5827910ac1b426545127dbe006e764cf2c03
Irwan Cannady <irwancannadys@gmail.com>
2026-03-03 23:15:24 +0700

    Initial Commit

---
+++ AfharaAdzan/AfharaAdzanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfharaAdzanApp.swift
+//  AfharaAdzan
+//
+//  Created by irwan on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AfharaAdzanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
